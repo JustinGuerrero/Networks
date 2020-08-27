ead8e3108698132f30c1c7da4ea2dd06a4b493fa
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-08-27 14:04:35 -0600

    Simple socket sending "pickles" which are objects

--- Server.py
+++ Server.py
@@ -1,9 +1,10 @@
 import socket
+import time
+import pickle
 
 
 
-
-
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -12,5 +13,12 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has been established..")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
+
+    d = {1: "hi", 2: "sacrifice at the alter!"}
+
+    msg = pickle.dumps(d)
+
+
+    msg = bytes(f'{len(msg): < {HEADERSIZE}}', "utf-8" )+ msg
+
+    clientsocket.send(msg)
