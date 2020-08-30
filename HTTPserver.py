371a19a51e55565c537f1d04a1b8092d52d459b5
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-08-30 13:48:23 -0600

    UPDATE: added to the HTTPserver.py file

--- HTTPserver.py
+++ HTTPserver.py
@@ -1,17 +1,17 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class helloHandler(BaseHTTPRequestHandler):
+class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('Hello'.encode())
+        self.wfile.write(self.path[1:].encode())
 
 
 def main():
     PORT = 8000
-    server = HTTPServer(('', PORT), helloHandler)
+    server = HTTPServer(('', PORT), echoHandler)
     print('Server running on port %s' % PORT)
     server.serve_forever()
 
