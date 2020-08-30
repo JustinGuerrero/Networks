98de75703b821ec81a2fa8b04bb8de028169ccd3
Blake Stanger <blakestanger@gmail.com>
2020-08-30 11:59:19 -0600

    Added requirements to each class

--- Client.py
+++ Client.py
@@ -20,35 +20,38 @@ python client.py 128.111.52.245 5000
 
 import socket
 import pickle
+import sys
 
-HEADERSIZE = 10
+class Client:
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+    HEADERSIZE = 10
 
-while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 1234))
 
-    full_msg = b''
-    new_msg = True
     while True:
-        msg = s.recv(16)
-        if new_msg:
-            print(f"new message length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False
 
-        full_msg += msg
+        full_msg = b''
+        new_msg = True
+        while True:
+            msg = s.recv(16)
+            if new_msg:
+                print(f"new message length: {msg[:HEADERSIZE]}")
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
 
-        if len(full_msg) - HEADERSIZE == msglen:
-            print("full msg recieved")
-            print(full_msg[HEADERSIZE:])
+            full_msg += msg
 
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = b''
+            if len(full_msg) - HEADERSIZE == msglen:
+                print("full msg recieved")
+                print(full_msg[HEADERSIZE:])
+
+                d = pickle.loads(full_msg[HEADERSIZE:])
+                new_msg = True
+                full_msg = b''
 
 
-print(full_msg)
+    print(full_msg)
 
 
 
--- Server.py
+++ Server.py
@@ -15,24 +15,32 @@ ON START UP ACCEPT A PORT PARAMETER, THAT CLIENTS CONNECT TO (python server.PY 5
 import socket
 import time
 import pickle
+import sys
+
+class Server:
+
+    def runServer(self):
+        HEADERSIZE = 10
+
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(), int(sys.argv[1])))
+        s.listen(1000)
+
+        while True:
+            clientsocket, address = s.accept()
+            print(f"connection from {address} has been established..")
+
+            d = {1: "hi", 2: "sacrifice at the alter!"}
+
+            msg = pickle.dumps(d)
+
+
+            msg = bytes(f'{len(msg): < {HEADERSIZE}}', "utf-8" )+ msg
+
+            clientsocket.send(msg)
 
 
 
-HEADERSIZE = 10
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
-
-while True:
-    clientsocket, address = s.accept()
-    print(f"connection from {address} has been established..")
-
-    d = {1: "hi", 2: "sacrifice at the alter!"}
-
-    msg = pickle.dumps(d)
-
-
-    msg = bytes(f'{len(msg): < {HEADERSIZE}}', "utf-8" )+ msg
-
-    clientsocket.send(msg)
+smash = Server
+smash.runServer(smash)
