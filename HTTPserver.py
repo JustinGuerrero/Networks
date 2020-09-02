785813f842168b95e9721484a48f5989f534fe1d
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-09-02 08:28:47 -0600

    UPDATE: added to the HTTPserver.py file

--- HTTPserver.py
+++ HTTPserver.py
@@ -1,12 +1,43 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-
+newgame = ['Rock', 'Paper', 'Scissors']
 class echoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(self.path[1:].encode())
+        if self.path.endswith('/newgame'):
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+
+            output = ''
+            output += '<html><body>'
+            output += '<h1> Rock, Paper, Scissors</h1>'
+            output += '<h3><a href="/newgame/new"> Start New Game</a></h3>'
+            for game in newgame:
+                output += game
+                output += '</br>'
+            output += '</body></html>'
+
+        if self.path.endswith('/newgame'):
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+
+            output += ''
+            output += '<html><body>'
+            output += '<h1> Enter your play</h1>'
+            output += '<form method="POST" enctype="multipart/form-data" action=/newgame/new">'
+            output += '<input name="move" type="text" placeholder="Enter move">'
+            output += '<input type="submit" value="add">'
+            output += '</form>'
+            output += '</body>'
+
+            self.wfile.write(output.encode())
+
+
+        # self.send_response(200)
+        # self.send_header('content-type', 'text/html')
+        # self.end_headers()
+        # self.wfile.write(self.path[1:].encode())
 
 
 def main():
