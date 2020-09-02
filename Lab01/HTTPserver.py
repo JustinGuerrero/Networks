0782733b9a17a311b97216988b4d2a4c0f45fc2b
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-09-02 09:04:17 -0600

    UPDATE: Placing all files into their own directory for lab one to avoid clutter.

--- Lab01/HTTPserver.py
+++ Lab01/HTTPserver.py
@@ -33,10 +33,7 @@ class echoHandler(BaseHTTPRequestHandler):
 
             self.wfile.write(output.encode())
 
-        # self.send_response(200)
-        # self.send_header('content-type', 'text/html')
-        # self.end_headers()
-        # self.wfile.write(self.path[1:].encode())
+
 
 
 def main():
