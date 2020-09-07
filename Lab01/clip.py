a57d51d6d450406d4f9d31bdffc2d53c85c52195
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-09-07 14:51:26 -0600

    UPDATE: commiting my side of things

--- Lab01/clip.py
+++ Lab01/clip.py
@@ -1,18 +1,20 @@
 import socket
 import sys
 import http.client
-import http.server
+import requests
 
 BUFFER_SIZE = 1024
 host = sys.argv[1]
 port = int(sys.argv[2])
+string = 'http://'+host+':'+sys.argv[2]
 print("Put some stupid thing here")
 answer = input()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.connect((host, port))
+
 s.send(bytes(answer, 'utf-8'))
 data = s.recv(BUFFER_SIZE)
-print(data)
+data2 = data.decode('utf-8')
+print(data2)
 s.close()
--- Lab01/serv.py
+++ Lab01/serv.py
@@ -1,28 +1,27 @@
 import socket
 import sys
-import http.client
 import http.server
 BUFFER_SIZE = 1024
-file = open("newFile.txt", "w")
+
 host = sys.argv[1]
 port = int(sys.argv[2])
 class Connect():
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    s.listen(1)
-
-
-
-    conn, addr = s.accept()
-    data = conn.recv(BUFFER_SIZE)
-    conn.send(data) #echo
-    print(data)
-    print(BUFFER_SIZE)
-    newData = data.decode('utf-8')
-    file.write(newData)
-    file.close()
-    #conn.close()
-
+    def socketStuff(self):
+        while(1):
+            file = open("newFile.txt", "a")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((host, port))
+            s.listen(1)
+            conn, addr = s.accept()
+            data = conn.recv(BUFFER_SIZE)
+            conn.send(data) #echo
+            print(data)
+            print(BUFFER_SIZE)
+            newData = data.decode('utf-8')
+            file.write(newData + '\n')
+            file.close()
+        conn.close()
+    # def do_get(self):
+    #     self.
 class main():
-    Connect()
+    Connect().socketStuff()
